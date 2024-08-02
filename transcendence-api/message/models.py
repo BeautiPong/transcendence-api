c5dd6b23e9936b70e36c9d0c4eb9d0874b64b58f
choiseoji <choiseoji123@naver.com>
2024-08-02 12:10:59 +0900

    Refact: Message에서 room_id -> room, sender_id -> sender로 변수 수정

--- transcendence-api/message/models.py
+++ transcendence-api/message/models.py
@@ -3,7 +3,7 @@ from chattingRoom.models import ChattingRoom
 from users.models import CustomUser
 
 class Message(models.Model):
-    room_id = models.ForeignKey(ChattingRoom, on_delete=models.CASCADE)
-    sender_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    room = models.ForeignKey(ChattingRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
