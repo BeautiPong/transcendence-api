05bbdbb606c2aba0419f86ce0ec2dbd0ab4522b2
choiseoji <choiseoji123@naver.com>
2024-08-06 15:21:14 +0900

    Feat: 친구 정보 반환 추가

--- transcendence-api/friend/views.py
+++ transcendence-api/friend/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
 from friend.models import Friend
+from rest_framework.response import Response
+from users.utils import *
 
 # 내 친구 리스트 반환
 class FriendList(APIView) :
@@ -14,17 +15,13 @@ class FriendList(APIView) :
     def get(self, request):
         user = request.user
 
-        # user1이 user인 column 찾아오기
-        # user2가 친구인데, status가 ACCEPT인 친구만
-        friend_with_user1 = Friend.objects.get(user1 = user)
+        friend_with_user1 = Friend.objects.filter(user1 = user, status=Friend.Status.ACCEPT)
         friend_list = [friend.user2 for friend in friend_with_user1]
 
-        response = JsonResponse(
-            {
-                "friend id" : friend_list.id,
-                "friend_name" : friend_list.name,
-            },
-            status = status.HTTP_200_OK
-        )
-        return response
+        friend_info_list = [ ]
+        for friend in friend_list :
+            friend_info_list.append(get_user_info(friend.nickname))
+
+        friend_info_serializer = UserInfoSerializer(friend_info_list, many=True)
+        return Response(friend_info_serializer.data, status=status.HTTP_200_OK)
 
